ad96b905a6ec6accad5c3f766cba6903b5d8399d
Peter <psmith2610@gmail.com>
2012-10-26 22:30:49 +0100

    Merged current CMS into Master.

--- application/php/RequestHandler/CMS.php
+++ application/php/RequestHandler/CMS.php
@@ -4,6 +4,15 @@ namespace Application\RequestHandler;
 
 class CMS extends \Application\RequestHandler {
 	public function dispatch() {
-		echo "CMS";
+		$xsl_dom = new \DOMDocument();
+		$xsl_dom->load("application/xsl/default.xsl");
+
+		$xml_dom = new \DOMDocument();
+		$xml_dom->load("application/xml/default.xml");
+
+		$xslt = new \XSLTProcessor();
+		$xslt->importStylesheet($xsl_dom);
+
+		echo $xslt->transformToXML($xml_dom);
 	}
 }
---
+++ application/xsl/default.xsl
@@ -0,0 +1,13 @@
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output method="html" doctype-system="about:legacy-compat" encoding="UTF-8" indent="no" />
+	<xsl:template match="/">
+		<html>
+			<head>
+				<title>Hello World</title>
+			</head>
+			<body>
+				<h1>Hello World</h1>
+			</body>
+		</html>
+	</xsl:template>
+</xsl:stylesheet>
